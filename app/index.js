5048f5ad935a64cb39b69368ff8fdaf32340fb2b
zyang <kero_zhao@163.com>
2017-08-28 21:52:50 +0800

    import jquery

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import $ from 'jquery'
 
 function component () {
-  var element = document.createElement('div');
+  var element = $('<div></div>');
 
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.html(_.join(['Hello','webpack'], ' '));
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
