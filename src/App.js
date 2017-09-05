4a00ed1960aa795e62ab8bc8e507e616204f4dd8
zyang <kero_zhao@163.com>
2017-09-05 22:10:54 +0800

    addtodo时调用todoModel.create

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import UserDialog from './UserDialog'
-import { getCurrentUser, signOut } from './leanCloud'
+import { getCurrentUser, signOut, TodoModel } from './leanCloud'
 
 class App extends Component {
   constructor(props){
@@ -61,15 +61,20 @@ class App extends Component {
   }
   componentDidUpdate(){}
   addTodo(event){
-    this.state.todoList.push({
-      id: idMaker(),
+    let newTodo = {
       title: event.target.value,
       status: null,
       deleted: false
-    })
-    this.setState({
-      newTodo: '',
-      todoList: this.state.todoList
+    }
+    TodoModel.create(newTodo, (id)=> {
+      newTodo.id = id;
+      this.state.todoList.push(newTodo);
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
+    }, (error)=>{
+      console.log(error);
     })
   }
   changeTitle(event){
@@ -89,10 +94,3 @@ class App extends Component {
 }
 
 export default App;
-
-let id = 0;
-
-function idMaker(){
-  id += 1
-  return id
-}
